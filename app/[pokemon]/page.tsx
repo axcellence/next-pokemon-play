7295482d56150ddd30ff9077f92fc7d06849716c
Axl Crawford <hi@axlcrawford.com>
2022-10-28 08:15:59 +0100

    Change image to front and update background color

--- app/[pokemon]/page.tsx
+++ app/[pokemon]/page.tsx
@@ -8,40 +8,41 @@ interface Props {
 }
 
 const colors = {
-  grass: "bg-green-300",
-  fire: "bg-red-300",
-  water: "bg-blue-300",
-  bug: "bg-green-300",
-  normal: "bg-gray-300",
-  poison: "bg-purple-300",
-  electric: "bg-yellow-300",
-  ground: "bg-yellow-300",
-  fairy: "bg-pink-300",
-  fighting: "bg-red-300",
-  psychic: "bg-pink-300",
-  rock: "bg-gray-300",
-  ghost: "bg-purple-300",
-  ice: "bg-blue-300",
-  dragon: "bg-purple-300",
-  flying: "bg-blue-300",
-  dark: "bg-gray-300",
-  steel: "bg-gray-300",
+  grass: "bg-gradient-to-br from-green-300 to-greem-600",
+  fire: "bg-gradient-to-br from-red-300 to-red-600",
+  water: "bg-gradient-to-br from-blue-300 to-blue-600",
+  bug: "bg-gradient-to-br from-emerald-300 to-emerald-600",
+  normal: "bg-gradient-to-br from-slate-300 to-slate-600",
+  poison: "bg-gradient-to-br from-purple-300 to-purple-600",
+  electric: "bg-gradient-to-br from-yellow-300 to-yellow-600",
+  ground: "bg-gradient-to-br from-amber-300 to-amber-600",
+  fairy: "bg-gradient-to-br from-pink-300 to-pink-600",
+  fighting: "bg-gradient-to-br from-orange-300 to-blue-600",
+  psychic: "bg-gradient-to-br from-pink-300 to-pink-600",
+  rock: "bg-gradient-to-br from-gray-300 to-gray-600",
+  ghost: "bg-gradient-to-br from-indigo-300 to-indigo-600",
+  ice: "bg-gradient-to-br from-cyan-300 to-cyan-600",
+  dragon: "bg-gradient-to-br from-violet-300 to-violet-600",
+  flying: "bg-gradient-to-br from-blue-300 to-blue-600",
+  dark: "bg-gradient-to-br from-gray-300 to-gray-600",
+  steel: "bg-gradient-to-br from-gray-300 to-gray-600",
 };
 
 export default async function PokemonPage({ params }: Props) {
   const pokemon = (await fetch(
     `https://pokeapi.co/api/v2/pokemon/${params.pokemon}`
   ).then((res) => res.json())) as Pokemon;
-  console.log(pokemon);
-  console.log(pokemon.sprites.other["official-artwork"]);
 
   const mainType = pokemon.types[0].type.name;
+  const image =
+    pokemon.sprites.other["official-artwork"].front_default ||
+    pokemon.sprites.back_default;
 
   return (
     <div className="flex gap-4">
       <div className={`rounded-full p-4 w-40 h-40 ${colors[mainType]}`}>
         <Image
-          src={pokemon.sprites.back_default}
+          src={image}
           width={100}
           height={100}
           alt={pokemon.name}
